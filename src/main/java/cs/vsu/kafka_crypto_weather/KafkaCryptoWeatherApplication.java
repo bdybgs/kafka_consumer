a57eed3ebd4170e5060ca60851d83a88d5f48d72
bdybgs <bondarev.mc.2018@gmail.com>
2024-07-10 12:26:19 +0300

    Initialized project

---
+++ src/main/java/cs/vsu/kafka_crypto_weather/KafkaCryptoWeatherApplication.java
@@ -0,0 +1,13 @@
+package cs.vsu.kafka_crypto_weather;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaCryptoWeatherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaCryptoWeatherApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cs/vsu/kafka_crypto_weather/KafkaCryptoWeatherApplicationTests.java
@@ -0,0 +1,13 @@
+package cs.vsu.kafka_crypto_weather;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaCryptoWeatherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
