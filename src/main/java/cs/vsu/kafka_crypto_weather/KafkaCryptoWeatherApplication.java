0b7e8038b59a42e8f61154ed4c1f0abad3d1f3f9
egor_egor <egorgolovin1@mail.ru>
2024-07-19 15:49:50 +0300

    configure db + CryptoWeatherService

--- src/main/java/cs/vsu/kafka_crypto_weather/KafkaCryptoWeatherApplication.java
+++ src/main/java/cs/vsu/kafka_crypto_weather/KafkaCryptoWeatherApplication.java
@@ -2,6 +2,7 @@ package cs.vsu.kafka_crypto_weather;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 public class KafkaCryptoWeatherApplication {
--- src/main/java/cs/vsu/kafka_crypto_weather/repository/CryptoManualRepository.java
+++ src/main/java/cs/vsu/kafka_crypto_weather/repository/CryptoManualRepository.java
@@ -4,8 +4,6 @@ import cs.vsu.kafka_crypto_weather.entity.CryptoManual;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-import java.util.Set;
 import java.util.UUID;
 
 @Repository
